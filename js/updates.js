673e8a521f2864a71842a830704f9a7ebd435880
johannghub <90212860+jtgithub16@users.noreply.github.com>
2023-11-22 00:17:27 -0500

    Add files via upload

---
+++ js/updates.js
@@ -0,0 +1,5 @@
+function redirectToWebsite(){    // var parentEl = document.querySelector("#git").parentElement;
+    // var imgEl = parentEl.innerHtml;
+    // parentEl.innerHtml = '<a href="https://github.com/jtgithub16/common_project">' + imgEl + '</a>';
+   window.location.href = 'https://github.com/jtgithub16/common_project';
+}
